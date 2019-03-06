c2eb839e00881a455646548095ef680fdfb1934a
Sebastian Maj <officialwolftech@gmail.com>
2019-03-05 21:55:57 -0500

    Added Mod and Lua Managers
    - Added ModManager Script
    - Added LuaManager Script
    - Added some basic Lua script management
    - Updated Unity to 2018.3.7f1

---
+++ Assets/Plugins/Modigine/Scripts/LuaManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class LuaManager : MonoBehaviour
+{
+	// This keeps track of an instance (Singleton)
+	public static LuaManager Instance;
+	
+	// A list of scripts to feed into the interpreter
+	public List<LuaScript> Scripts = new List<LuaScript>();
+
+	/// <summary>
+	/// Called upon awake
+	/// </summary>
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(gameObject);
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	/// <summary>
+	/// Add a new script to the list for the interpreter
+	/// </summary>
+	/// <param name="lua">The LuaScript object you'd like to add to runtime</param>
+	public void AddScript(string code)
+	{
+		LuaScript script = new LuaScript(code);
+		Scripts.Add(script);
+	}
+	
+	
+	/**
+	 * 	UNITY FUNCTIONS
+	 */
+
+	private void FixedUpdate()
+	{
+		Scripts.ForEach(x =>
+		{
+			if (x.GetScript().Globals.Get("FixedUpdate") != null)
+			{
+				x.GetScript().Call(x.GetScript().Globals.Get("FixedUpdate"));
+			}
+		});
+	}
+
+	private void Update()
+	{
+		Scripts.ForEach(x =>
+		{
+			if (x.GetScript().Globals.Get("Update") != null)
+			{
+				x.GetScript().Call(x.GetScript().Globals.Get("Update"));
+			}
+		});
+	}
+
+	private void LateUpdate()
+	{
+		Scripts.ForEach(x =>
+		{
+			if (x.GetScript().Globals.Get("LateUpdate") != null)
+			{
+				x.GetScript().Call(x.GetScript().Globals.Get("LateUpdate"));
+			}
+		});
+	}
+
+	private void OnGUI()
+	{
+		Scripts.ForEach(x =>
+		{
+			if (x.GetScript().Globals.Get("OnGUI") != null)
+			{
+				x.GetScript().Call(x.GetScript().Globals.Get("OnGUI"));
+			}
+		});
+	}
+}
---
+++ Assets/Plugins/Modigine/Scripts/ModManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ModManager : MonoBehaviour
+{
+    // This keeps track of an instance (Singleton)
+    public static ModManager Instance;
+
+    // Lua Manager
+    public LuaManager luaManager;
+    
+    /// <summary>
+    /// Called upon Awake
+    /// </summary>
+    private void Awake()
+    {
+        // Check to see if this is the only unique instance
+        if(Instance != null && Instance != this)
+            Destroy(gameObject);
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+        // Initialize Mod Manager
+        Initialize();
+    }
+
+    /// <summary>
+    /// Initializes the Mod Manager
+    /// </summary>
+    private void Initialize()
+    {
+        // Create a Lua Manager
+        GameObject luaManagerGo = new GameObject("Lua Manager");
+        luaManager = luaManagerGo.AddComponent<LuaManager>();
+        
+        
+        luaManager.AddScript("print('testing')");
+    }
+}
