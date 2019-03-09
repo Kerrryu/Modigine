2973bbc06b9179994af77c4d1c375c4f49a89f9a
Sebastian Maj <officialwolftech@gmail.com>
2019-03-09 15:21:48 -0500

    Began working on Register and Resource Mangager

---
+++ Assets/Plugins/Modigine/Scripts/RegisterManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Modigine
+{
+    public class RegisterManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Plugins/Modigine/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace
+{
+    public class ResourceManager : UnityEngine.MonoBehaviour
+    {
+        
+    }
+}
